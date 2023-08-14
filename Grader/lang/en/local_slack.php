1656cfeaab9e9ea6c1bab276f7a6af3a2c2ab81c
Moagg3 <61914353+Moagg3@users.noreply.github.com>
2023-08-14 04:26:54 +0200

    Add files via upload

---
+++ Grader/lang/en/local_slack.php
@@ -0,0 +1,16 @@
+<?php
+
+
+# This file is just declaring the names of all the different categories of data used
+
+$string['pluginname'] = 'Grader';
+
+$string['settings'] = 'Text Entry';
+$string['textentrylabel'] = 'Enter Text Here';
+
+$string['userdata'] = 'View Essay and Score';
+
+
+
+
+
